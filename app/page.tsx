d8fed1c104447ea861b6ebf1eaae4a336eb95f94
Kaan Demirekler <kaandemirekler@Bahars-MacBook-Air-2.local>
2025-12-12 11:23:09 -0500

    your message

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
         />
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xl text-4xl font-semibold mt-20 leading-10 tracking-tight text-black dark:text-zinc-50">
-            Hello World! 676 76 76 7 67 67 67 67 67
+            Hello World! 676 76 76 7 67 67 67 67 67. Alex N.
           </h1>
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
             Looking for a starting point or more instructions? Head over to Pravir R.{" "}
